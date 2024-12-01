b94fed4b20500fcbb420341168e7921856b80f92
Dmitry Shapran <djmitrofanov@gmail.com>
2024-12-01 20:20:44 +0300

    fix keywords 1.0.1

--- controller/keyWords.js
+++ controller/keyWords.js
@@ -2,77 +2,99 @@ const KeyWords = require('../models/KeyWords')
 
 class KeyWord {
 
-    async createKeyWord(req,res){
+    async createKeyWord(req, res) {
 
         try {
 
             const {keyword, position} = req.body
 
-            const newKeyWord = new KeyWords({
-                keyword, position
-            })
+            const key = await KeyWords.findOne({position: position})
 
-            await newKeyWord.save()
+            if (!key) {
+
+                const newKeyWord = new KeyWords({
+                    keyword, position
+                })
+
+                await newKeyWord.save()
+
+            } else {
+                return res.status(400).json({error: true, message: "The number exists", messageError:"The number exists"})
+            }
+
 
             res.status(201).json({error: false, message: "Key word successfully created"})
 
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             res.status(400).json({error: true, message: "Error service"})
         }
     }
 
-    async getAllKeywords(req,res){
+    async getAllKeywords(req, res) {
         try {
 
             const keywords = await KeyWords.find()
 
             res.status(201).json(keywords)
 
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             res.status(400).json({error: true, message: "Error service"})
         }
     }
 
-    async getKeywordId(req,res){
+    async getKeywordId(req, res) {
         try {
-            const keyword = await KeyWords.findOne({_id:req.params.id})
+            const keyword = await KeyWords.findOne({_id: req.params.id})
 
             res.status(201).json(keyword)
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             res.status(400).json({error: true, message: "Error service"})
         }
     }
 
-    async updateKeyword(req,res){
+    async updateKeyword(req, res) {
 
         try {
             const keyword = {
-                keyword:req.body.keyword,
-                position:req.body.position
+                keyword: req.body.keyword,
+                position: req.body.position
             }
-            const keyUp = await KeyWords.findOne({_id:req.params.id})
-            const key = await KeyWords.findOne({position:req.body.position})
+            const keyUp = await KeyWords.findOne({_id: req.params.id})
+            const key = await KeyWords.findOne({position: req.body.position})
 
-            if(keyUp && key){
-                await KeyWords.updateOne({_id:key._id},{$set:{position:keyUp.position}})
-                await KeyWords.updateOne({_id:req.params.id},{$set:keyword})
-            }else{
-                await KeyWords.updateOne({_id:req.params.id},{$set:keyword})
+            if (keyUp && key) {
+                await KeyWords.updateOne({_id: key._id}, {$set: {position: keyUp.position}})
+                await KeyWords.updateOne({_id: req.params.id}, {$set: keyword})
+            } else {
+                await KeyWords.updateOne({_id: req.params.id}, {$set: keyword})
             }
             res.status(200).json({error: false, message: "Update"})
 
 
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({error: true, message: "Error service"})
+        }
+    }
+
+    async deleteKeywordId(req,res){
+        try {
+
+
+           await KeyWords.deleteOne({_id:req.params.id})
+
+
+            res.status(200).json({error: false, message: "Delete keyword"})
+
         }catch (e) {
             console.log(e)
             res.status(400).json({error: true, message: "Error service"})
         }
     }
 
-
-
-
 }
+
 module.exports = new KeyWord()
--- routers/keyWorlds.js
+++ routers/keyWorlds.js
@@ -6,7 +6,8 @@ const Keywords = require('../controller/keyWords')
 router.get('/',Keywords.getAllKeywords)
 router.get('/:id',Keywords.getKeywordId)
 router.post('/keywords', Keywords.createKeyWord)
-router.patch('/:id',Keywords.updateKeyword)
+router.patch('/:id', Keywords.updateKeyword)
+router.delete('/:id', Keywords.deleteKeywordId)
 
 
 module.exports = router
